24467ce2933404323e845e346420bd8417ee10c7
Young Seo Kim <youngseokim7@gmail.com>
2025-02-25 16:16:51 +0900

    initial implementation of details view

---
+++ src/components/DetailsView.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { DetailsFormData } from "./DetailsForm";
+
+interface DetailsViewProps {
+  formData: DetailsFormData;
+}
+
+export default function DetailsView({ formData }: DetailsViewProps) {
+  const [data, setData] = useState<DetailsFormData>(formData);
+
+  useEffect(() => {
+    setData(formData);
+  }, [formData]);
+
+  return (
+    <div>
+      <div>{data.name}</div>
+      <div>{data.email}</div>
+      <div>{data.phone}</div>
+      <div>{data.address}</div>
+    </div>
+  );
+}
