c75114bc533b16336c9ed6179cdd393ba20f0d43
Young Seo Kim <youngseokim7@gmail.com>
2025-02-25 15:39:11 +0900

    initial implementation of education form component

---
+++ src/components/EducationForm.tsx
@@ -0,0 +1,66 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface EducationFormData {
+  school: string;
+  degree: string;
+  date: string;
+  location: string;
+}
+
+export default function EducationForm() {
+  const [formData, setFormData] = useState<EducationFormData>({
+    school: "",
+    degree: "",
+    date: "",
+    location: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    //
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <div className="text-3xl font-semibold">Education</div>
+      <label htmlFor="school">School Name</label>
+      <input
+        type="text"
+        name="school"
+        id="school"
+        onChange={handleChange}
+        value={formData.school}
+        required
+      />
+      <label htmlFor="degree">Degree</label>
+      <input
+        type="text"
+        name="degree"
+        id="degree"
+        onChange={handleChange}
+        value={formData.degree}
+        required
+      />
+      <label htmlFor="date">Graduation Date</label>
+      <input type="date" name="date" id="date" />
+      <label htmlFor="location">Location</label>
+      <input
+        type="text"
+        name="location"
+        id="location"
+        onChange={handleChange}
+        value={formData.location}
+        required
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import DetailsForm from "./components/DetailsForm";
+import EducationForm from "./components/EducationForm";
 //import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="border border-red-500 w-1/3">
       <DetailsForm />
+      <EducationForm />
     </div>
     <div className="border border-blue-500 w-2/3"></div>
   </StrictMode>,
