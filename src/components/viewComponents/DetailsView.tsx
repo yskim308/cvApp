a2d7c0c6f7733dc9e63572e95c69046d0e3a82ad
Young Seo Kim <youngseokim7@gmail.com>
2025-02-26 11:13:18 +0900

    make default be passed down from parent

--- src/components/viewComponents/DetailsView.tsx
+++ src/components/viewComponents/DetailsView.tsx
@@ -1,20 +1,13 @@
 import { DetailsViewProps } from "../types";
 
-export default function DetailsView({
-  formData = {
-    name: "John Doe",
-    email: "john@gmail.com",
-    phone: "999999999",
-    address: "5 Sigma Lane, Olympus",
-  },
-}: DetailsViewProps) {
+export default function DetailsView({ formData }: DetailsViewProps) {
   return (
     <div className="flex flex-col">
       <div className="text-2xl font-semibold">Details</div>
-      <div>name: {formData.name}</div>
-      <div>email: {formData.email}</div>
-      <div>phone: {formData.phone}</div>
-      <div>address: {formData.address}</div>
+      <div>name: {formData?.name}</div>
+      <div>email: {formData?.email}</div>
+      <div>phone: {formData?.phone}</div>
+      <div>address: {formData?.address}</div>
     </div>
   );
 }
