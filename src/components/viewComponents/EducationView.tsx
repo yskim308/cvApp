662419515ac711e6b4346a44e4ff092b40d30116
Young Seo Kim <youngseokim7@gmail.com>
2025-02-27 20:58:34 +0900

    style education view component

--- src/components/viewComponents/EducationView.tsx
+++ src/components/viewComponents/EducationView.tsx
@@ -1,13 +1,28 @@
 import { EducationViewProps } from "../types";
+import location from "../../assets/map-marker.svg";
+import graduationIcon from "../../assets/graduationIcon.svg";
 
 export default function EducationView({ formData }: EducationViewProps) {
   return (
     <div className="flex flex-col">
-      <div className="text-2xl font-semibold">Education</div>
-      <div>School: {formData?.school}</div>
-      <div>Degree: {formData?.degree}</div>
-      <div>Date: {formData?.date}</div>
-      <div>Location: {formData?.location}</div>
+      <div className="text-xl font-medium">Education</div>
+      <hr></hr>
+      <div className="flex justify-between">
+        <div className="flex">
+          <span className="font-semibold">{formData?.school}</span> -{" "}
+          {formData?.degree}
+        </div>
+        <div className="flex">
+          <div className="flex">
+            <img className="w-5" src={location} />
+            {formData?.location}
+          </div>
+          <div className="px-5 flex">
+            <img className="w-5 mr-1" src={graduationIcon} />
+            {formData?.date}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
