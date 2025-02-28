6843bf91728ce8fbc46346004c12a33df51fe8c5
Young Seo Kim <youngseokim7@gmail.com>
2025-02-28 11:16:53 +0900

    make education a list view

--- src/components/viewComponents/EducationView.tsx
+++ src/components/viewComponents/EducationView.tsx
@@ -2,27 +2,34 @@ import { EducationViewProps } from "../types";
 import location from "../../assets/map-marker.svg";
 import graduationIcon from "../../assets/graduationIcon.svg";
 
-export default function EducationView({ formData }: EducationViewProps) {
+export default function EducationView({
+  formData,
+  handleEducationDelete,
+}: EducationViewProps) {
   return (
     <div className="flex flex-col mt-5">
       <div className="text-xl font-medium">Education</div>
       <hr></hr>
-      <div className="flex justify-between">
-        <div className="flex">
-          <span className="font-semibold">{formData?.school}</span> -{" "}
-          {formData?.degree}
-        </div>
-        <div className="flex">
-          <div className="flex">
-            <img className="w-5" src={location} />
-            {formData?.location}
+      {formData?.map((school) => {
+        return (
+          <div className="flex justify-between">
+            <div className="flex">
+              <span className="font-semibold">{school?.school}</span> -{" "}
+              {school?.degree}
+            </div>
+            <div className="flex">
+              <div className="flex">
+                <img className="w-5" src={location} />
+                {school?.location}
+              </div>
+              <div className="px-5 flex">
+                <img className="w-5 mr-1" src={graduationIcon} />
+                {school?.date}
+              </div>
+            </div>
           </div>
-          <div className="px-5 flex">
-            <img className="w-5 mr-1" src={graduationIcon} />
-            {formData?.date}
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
