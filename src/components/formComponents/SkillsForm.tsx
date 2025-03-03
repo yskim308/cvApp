8b22ea744cb8bfd985b169f9ff3fc5d5d973b557
Young Seo Kim <youngseokim7@gmail.com>
2025-03-03 11:20:00 +0900

    add submit button, style, and fix bug with form update on submit

--- src/components/formComponents/SkillsForm.tsx
+++ src/components/formComponents/SkillsForm.tsx
@@ -26,13 +26,18 @@ export default function SkillsForm({ appendSkill }: SkillsFormProps) {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col border border-slate-300 px-5 pt-5 rounded-4xl text-slate-700 bg-slate-50"
+      >
+        <div className="text-xl font-semibold">Skills</div>
         <label htmlFor="skill">Skill</label>
         <input
           name="skill"
           id="skill"
           type="text"
           onChange={handleChange}
+          value={formData.skill}
           autoComplete="off"
           className="border h-7 py-2 px-1 rounded-lg"
           required
@@ -43,10 +48,19 @@ export default function SkillsForm({ appendSkill }: SkillsFormProps) {
           id="description"
           type="text"
           onChange={handleChange}
+          value={formData.description}
           autoComplete="off"
           className="border h-7 py-2 px-1 rounded-lg"
           required
         />
+        <div className="flex justify-center p-1">
+          <button
+            type="submit"
+            className="border border-slate-400 p-1 rounded-xl text-slate-700"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
